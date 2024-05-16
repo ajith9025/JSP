aa99ec93168475f84789c1165da896f51d0de689
ajith9025 <165891992+ajith9025@users.noreply.github.com>
2024-05-16 11:09:24 +0530

    Add files via upload

--- src/main/java/com/chainsys/ChainsysA.java
+++ src/main/java/com/chainsys/ChainsysA.java
@@ -1,7 +1,7 @@
 package com.chainsys;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,9 +30,27 @@ public class ChainsysA extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("In get method");
+		
+		//System.out.println("In get method");
+	  String firstname= request.getParameter("firstname");
+	  String lastname= request.getParameter("lastname");
+	  String password= request.getParameter("password");
+	  String confirmpassword= request.getParameter("confirm password");
+	  String email= request.getParameter("email");
+	  String contactno= request.getParameter("contact no");
+	  PrintWriter out=response.getWriter();
+	  out.println(firstname);
+	  out.println(lastname);
+	  out.println(password);
+	  out.println(confirmpassword);
+	  out.println(email);
+	  out.println(contactno);
+	  
+	  
 		
 	}
+		
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -40,7 +58,7 @@ public class ChainsysA extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	   System.out.println("In post method");
+		//System.out.println("In get method");
 	}
 
 }
